52149c4c106c4f4c1f720105323083b0df6ec3ca
Jonn Mostovoy <jm@serokell.io>
2020-10-03 03:20:31 +0100

    Problem: Single-display setup is distinct
    Solution: Include single-display setup in the repo, write a script to switch.

---
+++ xmobar-dual-mine.hs
@@ -0,0 +1,57 @@
+-- xmobar config used by Vic Fryzel
+-- Author: Vic Fryzel
+-- https://github.com/vicfryzel/xmonad-config
+
+-- This xmobar config is for a single 4k display (3840x2160) and meant to be
+-- used with the stalonetrayrc-single config.
+--
+-- If you're using a single display with a different resolution, adjust the
+-- position argument below using the given calculation.
+Config {
+    -- Position xmobar along the top, with a stalonetray in the top right.
+    -- Add right padding to xmobar to ensure stalonetray and xmobar don't
+    -- overlap. stalonetrayrc-single is configured for 12 icons, each 23px
+    -- wide.
+    -- right_padding = num_icons * icon_size
+    -- right_padding = 12 * 23 = 276
+    -- Example: position = TopP 0 276
+    --position = TopP 0 0,
+    position = Static { xpos = 1920, ypos = 0, width = 1920, height = 19 },
+    -- TODO: Integrate stalonetray
+    font = "xft:monospace-8",
+    bgColor = "#000000",
+    fgColor = "#ffffff",
+    lowerOnStart = False,
+    overrideRedirect = False,
+    allDesktops = False,
+    persistent = True,
+    commands = [
+        Run Weather "KPAO" ["-t","<tempF>F <skyCondition>","-L","64","-H","77","-n","#CEFFAC","-h","#FFB6B0","-l","#96CBFE"] 36000,
+        Run MultiCpu ["-t","Cpu: <total0> <total1> <total2> <total3>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
+        Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Network "wlp3s0" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Com "date" ["+\"%a %b %_d %H:%M\""] "mydate" 590,
+        --Run Date "%a %b %_d %T" "date" 10,
+        Run Com "getMasterVolume" [] "volumelevel" 10,
+        Run Battery        [ "--template" , "Batt: <acstatus>"
+                             , "--Low"      , "10"        -- units: %
+                             , "--High"     , "80"        -- units: %
+                             , "--low"      , "darkred"
+                             , "--normal"   , "darkorange"
+                             , "--high"     , "darkgreen"
+
+                             , "--" -- battery specific options
+                                       -- discharging status
+                                       , "-o"	, "<left>% (<timeleft>)"
+                                       -- AC "on" status
+                                       , "-O"	, "<fc=#dAA520>Charging</fc>"
+                                       -- charged status
+                                       , "-i"	, "<fc=#006000>Charged</fc>"
+                             ] 60,
+        Run StdinReader
+    ],
+    sepChar = "%",
+    alignSep = "}{",
+    template = "%StdinReader% }{ %multicpu%   %memory%   %swap%  %wlp3s0%   Vol: <fc=#b2b2ff>%volumelevel%</fc>  %battery%   <fc=#FFFFCC>%mydate%</fc>"
+}
